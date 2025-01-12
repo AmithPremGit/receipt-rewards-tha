b6c56b64be8ba132185ec967a9ccdc2a3334a961
Amith <amithpremnagandla@gmail.com>
2025-01-12 16:50:41 -0500

    add initial models and handler structure

--- handlers.go
+++ handlers.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"net/http"
+	"sync"
+)
+
+type Handlers struct {
+	receipts map[string]Receipt
+	points   map[string]int
+	mutex    sync.RWMutex
+}
+
+func NewHandlers() *Handlers {
+	return &Handlers{
+		receipts: make(map[string]Receipt),
+		points:   make(map[string]int),
+	}
+}
+
+// Add the function signatures - TODO: implement
+func (h *Handlers) ProcessReceipt(w http.ResponseWriter, r *http.Request) {}
+func (h *Handlers) GetPoints(w http.ResponseWriter, r *http.Request)      {}
+func calculatePoints(receipt Receipt) int                                 {}
+func validateReceipt(receipt Receipt) error                               {}
